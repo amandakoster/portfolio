c3199f3542bb95bb2aed67de0e299cbe3545af6d
Amanda Koster <amandakoster@gmail.com>
2017-05-11 14:55:26 -0700

    adding original content and original objects

--- scripts/rawData.js
+++ scripts/rawData.js
@@ -2,10 +2,16 @@
 
 var rawData = [
   {
-    title:       'Blog Title',
+    title:       'SalaamGarage',
     category:    'UX',
     author:      'Amanda Koster',
     authorUrl:   'http://www.amandakoster.com',
+    challenge:   '<p></p>'
+    approach:   '<p></p>'
+    process:    '<p></p>'
+    results:    '<p></p>'
+    successFailure:'<p></p>'
+    // photos:
     publishedOn: 'some date',
     body:        '<p></p>'
   },
