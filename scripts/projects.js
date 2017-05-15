184f3c8b62d5755e7181542bd901de6eefded6a3
Amanda Koster <amandakoster@gmail.com>
2017-05-14 17:11:45 -0700

    fixed intenting issues

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var projects = [];
 
 function Project(rawDataObject) {
@@ -7,17 +6,14 @@ function Project(rawDataObject) {
     this[key] = rawDataObject[key];
   }
 };
-
 Project.prototype.toHtml = function() {
   var template = $('#project-template').html();
   var templateRender = Handlebars.compile(template);
   return templateRender(this);
 };
-
 projectObject.forEach(function(project) {
   projects.push(new Project(project));
 });
-
 projects.forEach(function(NewProjectObject) {
   $('#projects').append(NewProjectObject.toHtml());
 });
