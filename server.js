ec3427aff1813fe655ef2d1cbe985b2b0a8f7ca9
Amanda Koster <amandakoster@gmail.com>
2017-05-17 16:28:38 -0700

    adding a node server and revising the file structure

---
+++ server.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = processs.env.PORT || 300
