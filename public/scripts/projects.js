9eda87e020ca35e7150b8284e8d569bdf505471d
Amanda Koster <amandakoster@gmail.com>
2017-05-23 19:45:20 -0700

    catching up lesson 11

--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -66,26 +66,3 @@ Project.fetchAll = () => {
     })
   }
 };
-
-
-//Models
-// OLD WAY
-// Project.fetchAll = function() {
-//   if (localStorage.rawData) {
-//     let rawData = (JSON.parse(localStorage.rawData))
-//     Project.loadAll(rawData)
-//     Project.all.forEach(function(newProject) {
-//       $('#projects').append(newProject.toHtml());
-//     })
-//   } else {
-//     $.getJSON('/data/jsonDataset.json').then(function(data) {
-//       localStorage.rawData = (JSON.stringify(data))
-//       data.forEach(function(projectObject) {
-//         Project.all.push(new Project(projectObject))
-//       })
-//       Project.all.forEach(function(newProject) {
-//         $('#projects').append(newProject.toHtml());
-//       })
-//     })
-//   }
-// }
