bf9f30c7914f9ff6aac4c1015f3190a204356b9b
Amanda Koster <amandakoster@gmail.com>
2017-05-25 19:12:55 -0700

    adding pages and scripts for repro view - potfolio not working but I am exhausted

--- public/server.js
+++ public/server.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.listen(PORT, function() {
-  console.log('The server is setup and serving via localhost: ' + PORT);
+// app.listen(PORT, function() {
+//   console.log('The server is setup and serving via localhost: ' + PORT);
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: './public'});
 });
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
