89ff505dedf754b1883cb3b1ee36e4beeb50fb85
Amanda Koster <amandakoster@gmail.com>
2017-06-04 14:19:17 -0700

    revisions

--- public/scripts/controllers/aboutController.js
+++ public/scripts/controllers/aboutController.js
@@ -2,15 +2,9 @@
 var app = app || {};
 
 (function(module) {
-  const aboutController = {};
+  const repoController = {};
 
-  // Done cOMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
-  /*
-  1 This is showing the section with ID about and hiding the section with ID aticles.
-  2 Then it call the function app.repos.requestRepos which lives on repos.js. That function does a request to GITHUB for all repos and passes that into repos.all and passes that into repoview.index. Which lives repoView.js. That function calls UI which empties the UL. It then appends all of the repos as LI under the UL.
-  */
-  aboutController.index = () => {
+  repoController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
   };
--- server.js
+++ server.js
@@ -2,21 +2,20 @@
 
 const express = require('express');
 const pg = require('pg');
-const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 const conString = 'postgress://localhost:5432';
 const client = new pg.Client(process.env.DATABASE_URL || conString);
 
-client.connect();
-client.on('error', err => console.error(err));
+// client.connect();
+// client.on('error', err => console.error(err));
 
 app.use(express.static('./public'));
-
-app.listen(PORT, function() {
-  console.log('The server is setup and serving via localhost: ' + PORT);
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: './public'});
 });
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
